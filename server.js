13387ac35dc6d41b560a09c78adec9013dd600ec
is3388 <meiychin@yahoo.com>
2022-08-15 18:01:56 -0400

    Installed dependencies, added keys.js to gitignore file and set up route handler for google authentication

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 const express = require('express')
+const passport = require('passport')
+const keys = require('./config/keys')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-app.get('/', (req, res) => {
-    res.send({hi: 'there'})
-})
+// generic register to let passport know there is a new strategy available
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, (accessToken) => {
+    console.log(accessToken)
+}))
+// passport.use(new FacebookStrategy({}))
+// first arg is which strategy provider
+// google server only give permission to access user's profile and email
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}))
 
 const PORT = process.env.PORT || 5000
 
