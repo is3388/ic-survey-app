c0670ed2aebb4896ddde51a73055bc55697e4fbd
is3388 <meiychin@yahoo.com>
2022-08-16 19:08:23 -0400

    Installed mongoose, wired up mongoose to our app

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require('express')
 require('./services/passport') // make sure the configuration execute once and no need to export anything, so no variable
 const authRouter = require('./routes/authRoutes')
 const { passportConfig } = require('./services/passport')
+const keys = require('./config/keys')
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-passportConfig()
+passportConfig(keys.mongoURI)
 
 // authRoutes return a function and immediately call that function with the app object
 //require('./routes/authRoutes')(app)
