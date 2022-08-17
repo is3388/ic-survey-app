b7fabaab66ccd044b1520d689e17c655dcb600d4
is3388 <meiychin@yahoo.com>
2022-08-17 19:04:55 -0400

    Edited code to prevent existing user from creating again in database

--- services/passport.js
+++ services/passport.js
@@ -12,7 +12,16 @@ const User = mongoose.model('User')
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback' // after permission is granted to express server and invoke callback (2nd arg) redirect user
 }, (accessToken, refreshToken, profile, done) => { // done is to tell passport the task succeed or not
-    new User({googleId: profile.id}).save()
+    User.findOne({googleId: profile.id})
+    .then((existingUser) => {
+        if(existingUser) {
+            done(null, existingUser) // first arg is eroor object and 2nd arg is the user record
+        }
+        else {
+            new User({googleId: profile.id}).save() 
+             .then(user => done(null, user))
+        }
+    }  )
 }))
 
 // passport.use(new FacebookStrategy({options here})) for multiple strategy
