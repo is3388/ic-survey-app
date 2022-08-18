74531318040a46a6045d234a0f9aee5e95cf3c03
is3388 <meiychin@yahoo.com>
2022-08-18 17:34:08 -0400

    Installed cookie-session, enable cookie and let passport to handle authentication via cookies

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cookieSession = require('cookie-session') 
+const passport = require('passport') 
 const keys = require('./config/keys')
 require('./models/User')
 require('./services/passport') // make sure the configuration execute once and no need to export anything, so no variable
@@ -7,6 +9,18 @@ const authRouter = require('./routes/authRoutes')
 
 mongoose.connect(keys.mongoURI)
 const app = express()
+
+// enable cookie
+// maxAge is the expiration to the cookie in millisecond
+// keys array to be more secured that can randomly pick and sign, encrypt the cookie
+app.use(cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys:[keys.cookieKey]
+}))
+
+// tell passport to make use of cookies to handle authentication
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
