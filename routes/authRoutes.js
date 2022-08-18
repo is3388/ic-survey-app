47f01453baaf7667f2392e3fa4826ab340840687
is3388 <meiychin@yahoo.com>
2022-08-18 18:59:59 -0400

    Downgraded passport version due to incompatible with session-cookie and added one route to get back the logged in user for testing purpose

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,3 +1,4 @@
+const { application } = require('express')
 const express = require('express')
 const passport = require('passport')
 
@@ -24,6 +25,11 @@ router.get('/google', passport.authenticate('google', {
 }))
 // route handler to handle after permission is granted and redirect user to that URL express server
 router.get('/google/callback', passport.authenticate('google'))
+
+// route to test req.user property and see if user model exists
+router.get('/api/current_user', (req, res) => {
+    res.send(req.user)
+})
 
 module.exports = router
 
