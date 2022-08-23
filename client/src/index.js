22bcbc8ecebaf25a12efb405fab272f88bc91e4f
is3388 <meiychin@yahoo.com>
2022-08-23 16:06:10 -0400

    Created authReducer for updating the login state

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import reducers from './reducers'
 import App from './components/App'
 
-const store = createStore(() => [], {},applyMiddleware()) // 1st arg is combined reducer, 2nd is initial state
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
+const enhancer = composeEnhancers(
+  applyMiddleware()
+  // other store enhancers if any
+);
+const store = createStore(reducers, enhancer); // 1st arg is combined reducer, 2nd is initial state
+
+//const store = createStore(() => [], {},applyMiddleware()) 
 
 ReactDOM.render(
     <Provider store={store}><App /></Provider>, 
