c454cb45ee8da8fc6b590d147177fe9cebd43332
is3388 <meiychin@yahoo.com>
2022-08-24 10:38:02 -0400

    Installed axios, redux thunk and hook up in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import reducers from './reducers'
 import App from './components/App'
 import 'materialize-css/dist/css/materialize.min.css'
+import reduxThunk from 'redux-thunk'
 
 const composeEnhancers =
   typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
@@ -13,7 +14,7 @@ const composeEnhancers =
       })
     : compose;
 const enhancer = composeEnhancers(
-  applyMiddleware()
+  applyMiddleware(reduxThunk)
   // other store enhancers if any
 );
 const store = createStore(reducers, enhancer); // 1st arg is combined reducer, 2nd is initial state
