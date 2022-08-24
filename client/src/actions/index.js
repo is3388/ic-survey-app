e27aa7e2d8c244c42039eb670c3c96547f0d5d79
is3388 <meiychin@yahoo.com>
2022-08-24 16:03:01 -0400

    Refactored fetchUser function in actions/index.js and edited authReducer for return FETCH_USER either null, false or user model

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,11 +1,10 @@
 import axios from 'axios'
 import { FETCH_USER } from './types'
 
-export const fetchUser = () => {
+export const fetchUser = () => 
    // using redux thunk returns a function with dispatch from redux store instead of returns JS object with action type and payload
    // dispatch the action after response get back and forward to all reducers
-   return function (dispatch) {
-    axios.get('/auth/api/current_user') 
-      .then( res => dispatch({ type: FETCH_USER, payload: res}))
+    async (dispatch) => {
+    const {data} = await axios.get('/auth/api/current_user') 
+      dispatch({ type: FETCH_USER, payload: data})
     }
-}
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -5,9 +5,15 @@
     }
 }*/
 
-const authReducer = (state={}, action) => {
+import { FETCH_USER } from "../actions/types"
+
+// null meaning don't know if user is logged in or not. Request is pending.
+// returns either null or false (not logged in) or user model contains googleId
+const authReducer = (state=null, action) => {
     console.log(action)
     switch (action.type) {
+        case FETCH_USER:
+            return action.payload || false
         default: 
             return state
     }
