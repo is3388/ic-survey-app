8b65af335ab95aeab7680d27d8ff8e0d8787a4bc
is3388 <meiychin@yahoo.com>
2022-08-29 12:06:40 -0400

    Added extra line to build again

--- server.js
+++ server.js
@@ -47,3 +47,4 @@ if (process.env.NODE_ENV === 'production') {
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+// add extra line for build
