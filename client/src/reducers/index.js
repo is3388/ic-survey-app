995a20e0889054cf65ae604e67f77eca30ce134b
is3388 <meiychin@yahoo.com>
2022-09-01 10:19:07 -0400

    Installed redux-form and added reduxForm to combineReducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux"
 import authReducer from './authReducer'
+import { reducer as reduxForm } from 'redux-form' // rename the built-in reducer to reduxForm
 
 export default combineReducers({
-    auth: authReducer
+    auth: authReducer,
+    form: reduxForm
 })
