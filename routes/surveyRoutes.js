464f477db76ef0736d3f4e3e9b259409c2df1bd2
is3388 <meiychin@yahoo.com>
2022-09-05 17:26:13 -0400

    Installed lodash and path-parser. Implemented the code for webhook route

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -5,11 +5,42 @@ const mongoose = require('mongoose')
 const Survey = mongoose.model('Survey')
 const mailer = require('../services/Mailer')
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate')
+const _ = require('lodash')
+const { Path } = require('path-parser')
+const { URL } = require('url')
 
 const router = express.Router()
 
 router.get('/thanks', (req, res) => {
     res.send('Thanks for voting!')
+})
+
+router.post('/webhooks', (req, res) => { // req.body contains a list of events and event is event object, destructure it to url and email
+    const events = _.map(req.body, ({email, url}) => {
+        // extract the route with surveyId and choice
+        const pathname = new URL(url).pathname
+        // create a parser object that extracts every wildcard match up from the route which is survey id and choice or null(cannot extract)
+        const p = new Path('/api/surveys/:surveyId/:choice')
+        //console.log(p.test(pathname))
+        if (!url) {
+          return res.status(400).json({ error: 'Some error message' });
+        } else {
+
+                const match = p.test(pathname)
+                if (match) {
+                    return { email, 
+                             surveyId: match.surveyId,
+                             choice: match.choice
+                            }
+                            }
+                }
+        })
+    //console.log(events)
+    //iterate events and remove elements with undefined by using lodash compact and then uniqBy to remove duplicate elements
+    const compactEvents = _.compact(events)
+    const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId')
+    console.log(uniqueEvents)
+    res.send({}) // tell sendgrid everything is ok, no need to resend request to prevent duplicate
 })
 
 router.post('/', requireLogin, requireCredits, async (req, res) => {
