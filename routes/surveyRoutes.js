da9c3161cd05b5f485a9b4d35e47a8180de25938
is3388 <meiychin@yahoo.com>
2022-09-06 10:57:04 -0400

    Refactor code in route for webhooks

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -15,32 +15,34 @@ router.get('/thanks', (req, res) => {
     res.send('Thanks for voting!')
 })
 
-router.post('/webhooks', (req, res) => { // req.body contains a list of events and event is event object, destructure it to url and email
-    const events = _.map(req.body, ({email, url}) => {
-        // extract the route with surveyId and choice
-        const pathname = new URL(url).pathname
-        // create a parser object that extracts every wildcard match up from the route which is survey id and choice or null(cannot extract)
-        const p = new Path('/api/surveys/:surveyId/:choice')
-        //console.log(p.test(pathname))
+router.post('/webhooks', (req, res) => { 
+    // req.body contains a list of events and event is event object, destructure it to url and email
+    const p = new Path('/api/surveys/:surveyId/:choice')
+    const events = _.chain(req.body) // iterate req.body and map it
+        .map(({email, url}) => {
+                
         if (!url) {
-          return res.status(400).json({ error: 'Some error message' });
-        } else {
-
-                const match = p.test(pathname)
-                if (match) {
-                    return { email, 
-                             surveyId: match.surveyId,
-                             choice: match.choice
-                            }
-                            }
+          return res.status(400).json({ error: 'url is undefined' });
+        } 
+        // extract the route with surveyId and choice
+        // create a parser object that extracts every wildcard match up from the route which is survey id and choice or null(cannot extract)
+        const match = p.test(new URL(url).pathname)
+        if (match) {
+            return { 
+                email, 
+                surveyId: match.surveyId,
+                choice: match.choice
                 }
+            }
+                
         })
-    //console.log(events)
-    //iterate events and remove elements with undefined by using lodash compact and then uniqBy to remove duplicate elements
-    const compactEvents = _.compact(events)
-    const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId')
-    console.log(uniqueEvents)
-    res.send({}) // tell sendgrid everything is ok, no need to resend request to prevent duplicate
+    
+    //iterate events and remove elements with undefined by using lodash compact 
+        .compact()
+        .uniqBy('email', 'surveyId') // lodash uniqBy to remove duplicate elements
+        .value() // return the value
+        console.log(events)
+        res.send({}) // tell sendgrid everything is ok, no need to resend request to prevent duplicate
 })
 
 router.post('/', requireLogin, requireCredits, async (req, res) => {
